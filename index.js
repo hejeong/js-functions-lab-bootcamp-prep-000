c38d603de6a6752e2bedda5829e954520eb5f1d1
Jonathan Hong <jonhong777@gmail.com>
2018-06-13 23:56:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,11 @@
 function happyHolidays(){
   return "Happy holidays!"
 }
+
+function happyHolidaysTo(name){
+  return `Happy Holidays, ${name}!`
+}
+
+function happyHolidayTo(holiday, name){
+  return `Happy ${holiday}, ${name}`
+}
